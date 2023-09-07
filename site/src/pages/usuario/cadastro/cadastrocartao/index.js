f18aaa5f4c2515a711c46bd2c559fa013902e207
solleil <tccsoleil028@gmail.com>
2023-09-07 12:20:04 -0300

    teste

---
+++ site/src/pages/usuario/cadastro/cadastrocartao/index.js
@@ -0,0 +1,12 @@
+import './index.scss'
+
+export default function Cadastrocartao(){
+    
+    
+    
+    return(
+        <div>
+                
+        </div>
+    )
+}
---
+++ site/src/pages/usuario/pagamento/index.js
@@ -0,0 +1,23 @@
+import './index.scss'
+import Cabecalho from '../../../components/cabecalho'
+
+export default function Escolherpagamaneto(){
+
+
+    return(
+        <div classname="pag-pagamento">
+            <div className="cab-pagamento">
+                <Cabecalho/>
+            </div>
+            <div className='trajeto'>
+
+            </div>
+            <div className='entrega'>
+                
+            </div>
+            <div className="met-pag">
+
+            </div>
+        </div>
+    )
+}
