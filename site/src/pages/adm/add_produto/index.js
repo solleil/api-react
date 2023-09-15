1084a088e5e56370b00612ab767ed95114e08ce9
solleil <tccsoleil028@gmail.com>
2023-09-14 22:29:46 -0300

    ok - KEWRY

--- site/src/pages/adm/add_produto/index.js
+++ site/src/pages/adm/add_produto/index.js
@@ -9,20 +9,40 @@ export default function AddProduto() {
         <Cabecalho/>
       </div>
 
+
       <div className='s-1-1'>
         <p>Adicionar Produto</p>
         <div className='l-1'></div>
       </div>
 
+
       <div className='s-2'>
         <div className='i-1'>
+          <p>Nome</p>
             <input type='text'></input>
+          <p>Preço</p>  
             <input type='text'></input>
         </div>
         <div className='i-2'>
+          <p>Descrição</p>
           <input type='text'></input>
         </div>
       </div>
+
+
+      <div className='s-3'>
+        <div className='a-1'>
+          <p>Adicionar imagem:</p>
+          <input type='text'></input>
+        </div>
+        <div className='c-1-1'>
+
+        </div>
+        <div className='c-2'>
+
+        </div>
+      </div>
+
     </div>
   );
 }
