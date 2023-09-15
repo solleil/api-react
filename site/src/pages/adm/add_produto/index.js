7cc25e527343334876a7ed63155cd7d67e765596
solleil <tccsoleil028@gmail.com>
2023-09-14 21:09:14 -0300

    ok - KEWRY

--- site/src/pages/adm/add_produto/index.js
+++ site/src/pages/adm/add_produto/index.js
@@ -1,9 +1,28 @@
 import './index.scss';
+import Cabecalho from '../../../components/cabecalho';
+
 
 export default function AddProduto() {
   return (
-    <div className="pag-add-produto">
-      <h1>oi</h1>
+    <div className="s-1">
+      <div className='c-1'>
+        <Cabecalho/>
+      </div>
+
+      <div className='s-1-1'>
+        <p>Adicionar Produto</p>
+        <div className='l-1'></div>
+      </div>
+
+      <div className='s-2'>
+        <div className='i-1'>
+            <input type='text'></input>
+            <input type='text'></input>
+        </div>
+        <div className='i-2'>
+          <input type='text'></input>
+        </div>
+      </div>
     </div>
   );
 }
