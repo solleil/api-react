f7504461b45e687de9e2fba5b1aee312ce19340a
Solleil <tccsoleil028@gmail.com>
2023-09-17 21:10:45 -0300

    fefa

--- site/src/pages/usuario/cadastro/cadastro/index.js
+++ site/src/pages/usuario/cadastro/cadastro/index.js
@@ -1,8 +1,14 @@
-
 import './index.scss';
-
+import { useState } from 'react';
 export default function Cadastro() {
-
+  const [nome, setNome] = useState('');
+  const [sobrenome, setSobrenome] = useState('');
+  const [cpf, setCpf] = useState('');
+  const [email, setEmail] = useState('');
+  const [senha, setSenha] = useState('');
+  const [confirmaSenha, setConfirmaSenha] = useState('');
+  const [dtNascimento, setDtNascimen] = useState('');
+  const [tipoPele, setTipoPele] = useState('');
 
 
   return (
@@ -16,23 +22,30 @@ export default function Cadastro() {
 
     <div className='c-1'>
       <div className='c-2'>
-        <p>Cadastro</p>
+        <p>CADASTRO</p>
 
         <div className='c-3'>
-          <input type='text'></input>
-          <input type='text'></input>
+          <label>Nome</label>
+          <input type='text' value={nome} onChange={(e) => setNome(e.target.value)}></input>
+          <label>Sobrenome</label>
+          <input type='text' value={sobrenome} onChange={(e) => setSobrenome(e.target.value)}></input>
         </div>
         <div className='c-4'>
-        <input type='text'></input>
-        <input type='text'></input>
-        <input type='text'></input>
-        <input type='text'></input>
+        <label>CPF</label>
+        <input type='text' value={cpf} onChange={(e) => setCpf(Number(e.target.value))}></input>
+        <label>E-MAIL</label>
+        <input type='text' value={email} onChange={(e) => setEmail(e.target.value)}></input>
+        <label>SENHA</label>
+        <input type='text' value={senha} onChange={(e) => setSenha(Number(e.target.value))}></input>
+        <label>CONFIRMAÇÃO DE SENHA</label>
+        <input type='text'value={confirmaSenha} onChange={(e) => setConfirmaSenha(Number(e.target.value))}></input>
         </div>
         <div className='c-5'>
-          <input type='text'></input>
+          <label>DATA NASCIMENTO</label>
+          <input type='text'>value={dtNascimento} onChange={(e) => setDtNascimen(Number(e.target.value))}</input>
         </div>
         <div className='c-6'>
-        <select className='tipodepele'>
+        <select className='tipodepele' value={tipoPele} onChange={(e) => setTipoPele(e.target.value)}>
             <option>Tipodepele</option>
             </select>
         </div>
