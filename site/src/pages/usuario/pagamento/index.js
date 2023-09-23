33f672b6669accadd46b4c058d0938a9f389d3e6
kewry <kewrybrito7@gmail.com>
2023-09-23 20:05:59 -0300

    ok-kewry

--- site/src/pages/usuario/pagamento/index.js
+++ site/src/pages/usuario/pagamento/index.js
@@ -5,7 +5,7 @@ export default function Escolherpagamento(){
 
 
     return(
-        <div classname="s-1">
+        <div classname='s-1'>
             <Cabecalho/>
             
             <div className='s-2'>
@@ -19,7 +19,7 @@ export default function Escolherpagamento(){
             
             <div className='s-3'>
                 <div className='s-1-3'>
-                    <p>Entrega</p>
+                    <p><strong>Entrega</strong></p>
                     <img src='/assets/images/pagamento/verificar.png' alt=''/>
                     <div className='s-2-3'> <p>Av. Coronel Octaviano de Freitas Costa, 463 - Socorro, São Paulo - SP, CEP: 04773-000</p></div>
                 </div>
@@ -27,8 +27,18 @@ export default function Escolherpagamento(){
             </div>
             
             
-            <div className="met-pag">
-
+            <div className='s-4'>
+                <p><strong>MÉTODO DE PAGAMENTO</strong></p>
+                <div>
+                    <img src='/assets/images/pagamento/cartao-de-credito.png' alt=''/> <p>Cartão de crédito</p>
+                  
+                </div>
+                <div>
+                    <img src='/assets/images/pagamento/boleto2.png' alt=''/>
+                </div>
+                <div>
+                    <img src='/assets/images/pagamento/cartao-de-debito.png' alt='' /> <p>Cartão de débito</p>
+                </div>
             </div>
         </div>
     )
