17d854975e95359bb4edc2795afc5b336840cca3
davicavalcantivital <davicvital@gmail.com>
2023-10-19 00:13:02 -0300

    ok davi

--- site/src/pages/adm/consulta_produto/index.js
+++ site/src/pages/adm/consulta_produto/index.js
@@ -1,44 +1,66 @@
+import { useState } from 'react';
 import CabecalhoAdm from '../../../components/cabecalhoAdm/index.js'
 import './index.scss';
-import { useState } from 'react';
 
 export default function Consulta() {
+  const [codigo, setCodigo] = useState('');
+  const [nomeProduto, setNomeProduto] = useState('');
+  const [categoria, setCategoria] = useState('');
+  const [status, setStatus] = useState('');
+  const [id, setId] = useState('');
+  const [marca, setMarca] = useState('');
+  const [filtro, setFiltro] = useState('');
+  const [desc, setDesc] = useState('');
+
+  const limparCampos = () => {
+    setCodigo('');
+    setNomeProduto('');
+    setCategoria('');
+    setStatus('');
+    setId('');
+    setMarca('');
+    setFiltro('');
+    setDesc('');
+  };
+
   return (
     <section className='index_consulta_produto'>
       <CabecalhoAdm />
       <header>
         <h1>Consulta de Produtos</h1>
-        <hr />
       </header>
+      <hr/>
       <div className='sec_1'>
         <div className='sec1_c1'>
           <div className='sec1c1-cx1'>
             <label>Código:</label>
-            <input id='input'/>
+            <input id='input' value={codigo} onChange={(e) => setCodigo(e.target.value)}/>
           </div>
           <div className='sec1c1-cx2'>
             <label>Nome do Produto:</label>
-            <input id='input'/>
+            <input id='input' value={nomeProduto} onChange={(e) => setNomeProduto(e.target.value)}/>
           </div>
         </div>
         <div className='sec1_c2'>
           <div className='sec1c2-cx1'>
             <label>Categoria:</label>
-            <input id='input'/>
+            <select id='select' value={categoria} onChange={(e) => setCategoria(e.target.value)}>
+              <option disabled selected>Selecione</option>
+            </select>
           </div>
           <div className='sec1c2-cx2'>
             <label>Status do Produto:</label>
-            <input id='input'/>
+            <input id='input' value={status} onChange={(e) => setStatus(e.target.value)}/>
           </div>
           <div className='sec1c2-cx3'>
             <label>ID do Usuário:</label>
-            <input id='input'/>
+            <input id='input' value={id} onChange={(e) => setId(e.target.value)}/>
           </div>
         </div>
         <div className='sec1_c3'>
           <div className='sec1c3-cx1'>
             <label>Marca:</label>
-            <input id='input'/>
+            <input id='input' value={marca} onChange={(e) => setMarca(e.target.value)}/>
           </div>
           <div className='sec1c3-cx2'>
             <label>Transportadora:</label>
@@ -46,18 +68,27 @@ export default function Consulta() {
           </div>
           <div className='sec1c3-cx3'>
             <label>Filtrar por preço:</label>
-            <input id='input'/>
+            <input id='input' value={filtro} onChange={(e) => setFiltro(e.target.value)}/>
           </div>
         </div>
         <div className='sec1_c4'>
           <div className='sec1c4-cx1'>
             <label>Descrição do Produto</label>
-            <textarea/>
+            <textarea value={desc} onChange={(e) => setDesc(e.target.value)}/>
           </div>
-          <div>
+          <div className='sec1c4-botoes'>
             <button>Procurar</button>
-            <button>Limpar Filtros</button>
+            <button onClick={limparCampos}>Limpar Filtros</button>
           </div>
+        </div>
+      </div>
+      <hr/>
+      <div className='sec_2'>
+        <header>
+          <h3>Resultado:</h3>
+        </header>
+        <div>
+          <h4>Sem resultados</h4>
         </div>
       </div>
     </section>
