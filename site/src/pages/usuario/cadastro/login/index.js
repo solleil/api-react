64d8b4f9edf7ce90bd6d54903e144e7c3a34f052
palomaasss <palomajonson207@gmail.com>
2023-10-26 18:27:38 -0300

    ok-paloma

--- site/src/pages/usuario/cadastro/login/index.js
+++ site/src/pages/usuario/cadastro/login/index.js
@@ -26,41 +26,49 @@ export default function Login() {
 
   return (
     <div className="index_login_usuario">
-      <div className='fundo'>
+    
+
         <div className='sec_1'>
+
           <div className='sec1_div_img'>
             <img src='/assets/images/usuario/login/tccsoleil2s2login_img.png' alt=''></img>
           </div>
+
           <div className='sec1_login'>
-            <div className='login_1'>
-              <div className='log1_titulos'>
-                <h2 id='solleil'>Solleil</h2>
-                <h2 id='login'>Faça seu Login</h2>
+           
+              <div className='s1-login'>
+                <p id='solleil'>Solleil</p>
+                <p id='login'>Faça seu Login</p>
               </div>
-              <div className='log1_inputs'>
+
+              <div className='s2-login'>
+
                 <div id='input_log1'>
                   <label>Email</label>
                   <input type='text' className='input_email' value={email} onChange={(e) => setEmail(e.target.value)}></input>
                 </div>
+
                 <div id='input_log1'>
                 <label>Senha</label>
                 <input type='password' className='input_senha' value={senha} onChange={(e) => setSenha(e.target.value)}></input>
                 </div>
+                
+                <button onClick={loginUsuario}>Continuar</button>
+                <h4>ou</h4>
+                <h6><Link to={'/cadastro'} id='pag_cadastro'>Não tem uma conta? Realize seu Cadastro.</Link></h6>
+
               </div>
-              <button onClick={loginUsuario}>Continuar</button>
-              <h6 id='reset_log1'>Esqueceu sua Senha?</h6>
-            </div>
-            <div className='login_2'>
-              <h4>ou</h4>
-            </div>
-            <div className='login_3'>
-              <button id='botao_face'>Continuar com Facebook</button>
-              <button>Continuar com Google</button>
-              <h6><Link to={'/cadastro'} id='pag_cadastro'>Não tem uma conta? Realize seu Cadastro.</Link></h6>
-            </div>
+
+
+             
+ 
+              
+          
           </div>
-        </div>    
-      </div>
+        </div>  
+
+
+
     </div>
   );
 }
