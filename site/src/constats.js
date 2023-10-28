d3709e8cffb853e682a83a7ceda68ded50e97b8a
Fernanda silva <129549862+FernandaSilvSou@users.noreply.github.com>
2023-10-28 12:22:02 -0300

    fer

--- site/src/constats.js
+++ site/src/constats.js
@@ -1,4 +1,4 @@
 
-//export const api_server = 'http://129.148.42.252:5027';
+export const api_server = 'http://129.148.42.252:5027';
 
-export const api_local = 'http://localhost:5000';
+//export const api_local = 'http://localhost:5000';
