3dac8f3fc9b8b94c28b08a99c626f5f7a44fd365
DaviCavalcantiVital <davicvital@gmail.com>
2023-11-01 18:26:49 -0300

    ok-davi

--- site/src/pages/usuario/cadastro/cadastro/index.js
+++ site/src/pages/usuario/cadastro/cadastro/index.js
@@ -6,27 +6,27 @@ export default function Cadastro() {
   return (
     <main className='index-cadastro'>
       <section className='fundo'>
-        <div className='backgroud_cadastro'>
+        <div className='background_cadastro'>
           <div className='tela_cadastro'>
             <h1>CADSATRO</h1>
-            <div>
-              <input placeholder='Nome'/>
-              <input placeholder='Sobrenome'/>
-            </div>
-            <div>
-              <input placeholder='CPF'/>
-              <input placeholder='E-mail'/>
-            </div>
-            <div>
-              <input placeholder='Senha'/>
-              <input placeholder='Confirme sua senha'/>
-            </div>
-            <div>
-              <input type='date'/>
-              <select>
-                <option></option>
-              </select>
-            </div>
+              <div id='in_puts'>
+                <input placeholder='Nome'/>
+                <input placeholder='Sobrenome'/>
+              </div>
+              <div>
+                <input placeholder='CPF'/>
+                <input placeholder='E-mail'/>
+              </div>
+              <div>
+                <input placeholder='Senha'/>
+                <input placeholder='Confirme sua senha'/>
+              </div>
+              <div className='seletores'>
+                <input type='date'/>
+                <select>
+                  <option>Selecione</option>
+                </select>
+              </div>
           </div>
         </div>
       </section>
