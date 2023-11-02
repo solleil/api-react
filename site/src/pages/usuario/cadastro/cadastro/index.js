876bc02f605ac95a3945a7e8f2b905079bf19652
davicavalcantivital <davicvital@gmail.com>
2023-11-02 11:01:56 -0300

    ok

--- site/src/pages/usuario/cadastro/cadastro/index.js
+++ site/src/pages/usuario/cadastro/cadastro/index.js
@@ -8,25 +8,26 @@ export default function Cadastro() {
       <section className='fundo'>
         <div className='background_cadastro'>
           <div className='tela_cadastro'>
-            <h1>CADSATRO</h1>
-              <div id='in_puts'>
-                <input placeholder='Nome'/>
-                <input placeholder='Sobrenome'/>
-              </div>
-              <div>
-                <input placeholder='CPF'/>
-                <input placeholder='E-mail'/>
-              </div>
-              <div>
-                <input placeholder='Senha'/>
-                <input placeholder='Confirme sua senha'/>
-              </div>
-              <div className='seletores'>
-                <input type='date'/>
-                <select>
-                  <option>Selecione</option>
-                </select>
-              </div>
+            <h1>CADASTRO</h1>
+            <div id='in_puts'>
+              <input placeholder='Nome' id='campos' type='text' />
+              <input placeholder='Sobrenome' id='campos' type='text' />
+            </div>
+            <div id='in_puts'>
+              <input placeholder='CPF' id='campos' type='text' />
+              <input placeholder='E-mail' type='text' id='campos' />
+            </div>
+            <div id='in_puts'>
+              <input placeholder='Senha' type='password' id='campos' />
+              <input placeholder='Confirme sua senha' type='password' id='campos' />
+            </div>
+            <div className='seletores'>
+              <input type='date' id='campos' className='campos_seletores' />
+              <select id='campos' className='campos_seletores'>
+                <option>Selecione</option>
+              </select>
+            </div>
+            <button>Cadastrar</button>
           </div>
         </div>
       </section>
