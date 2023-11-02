00f6c84630087d7fc8dffe1979783e79f102f07b
fernanda <fernandafernanda55.br@gmail.com>
2023-11-02 10:21:12 -0300

    fernanda- nao mexem no cadastro!

--- site/src/pages/usuario/cadastro/cadastro/index.js
+++ site/src/pages/usuario/cadastro/cadastro/index.js
@@ -1,36 +1,135 @@
 import './index.scss';
+import { Link} from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 export default function Cadastro() {
+  const [nome, setNome] = useState('');
+  const [sobrenome, setSobrenome] = useState('');
+  const [cpf, setCpf] = useState('');
+  const [email, setEmail] = useState('');
+  const [senha, setSenha] = useState('');
+  const [confirmaSenha, setConfirmaSenha] = useState('');
+  const [dtNascimento, setDtNascimen] = useState(0);
 
+
+  const [tipoPele, setTipoPele] = useState(0);
+  const [tipoPeleSelecionado, setPeleSelecionado] = useState([]);
+
+  
+
+  async function listarTiposdePele() {
+      
+      const r = await axios.get("http://localhost:5000/tipopele");
+      setPeleSelecionado(r.data);
+
+  }
+
+  
+  const cadastrar = () => {
+    setNome('')
+    setSobrenome('');
+    setCpf(0);
+    setEmail('');
+    setSenha(0);
+    setConfirmaSenha(0);
+    setDtNascimen(0);
+    setTipoPele(0);
+
+   
+   
+  }
+
+  useEffect(() =>{
+
+    listarTiposdePele()
+  }, [])
 
   return (
-    <main className='index-cadastro'>
-      <section className='fundo'>
-        <div className='background_cadastro'>
-          <div className='tela_cadastro'>
-            <h1>CADSATRO</h1>
-              <div id='in_puts'>
-                <input placeholder='Nome'/>
-                <input placeholder='Sobrenome'/>
-              </div>
-              <div>
-                <input placeholder='CPF'/>
-                <input placeholder='E-mail'/>
-              </div>
-              <div>
-                <input placeholder='Senha'/>
-                <input placeholder='Confirme sua senha'/>
-              </div>
-              <div className='seletores'>
-                <input type='date'/>
-                <select>
-                  <option>Selecione</option>
-                </select>
-              </div>
-          </div>
-        </div>
-      </section>
-    </main>
-  );
+    <div className='tude'>
 
+      <div className='c-0'>
+
+        <div className='fundo'>
+          <img src='/assets/images/geral/fe.png' alt='' />
+        </div>
+
+        <div className='c-1'>
+
+          <div className='c-2' >
+
+            <div className='cadastro'>
+            <p>CADASTRO</p>
+            </div>
+
+
+            <div className='c-3'>
+                
+                <div className='no-1'>
+                    
+                    <input type='Nome' id='nome'  placeholder="NOME" value={nome} onChange={ (e) => setNome(e.target.value)}></input>
+                </div>
+              
+                <div className='no-1'>
+                  
+                  <input type='text' id='sobrenome' placeholder="SOBRENOME" value={sobrenome} onChange={(e) => setSobrenome(e.target.value)}></input>
+                </div>
+             </div>
+
+            <div className='c-4'>
+              <div className='no-1'>
+              
+              <input type='text' id='cpf' placeholder="CPF" value={cpf} onChange={(e) => setCpf(Number(e.target.value))}></input>
+              </div>
+              <div className='no-1'>
+              
+              <input type='email' id='email' placeholder="E-MAIL" value={email} onChange={(e) => setEmail(e.target.value)}></input>
+              </div>
+              <div className='no-1'>
+              
+              <input type='password' id='senha' placeholder="SENHA" value={senha} onChange={(e) => setSenha(Number(e.target.value))}></input>
+              </div>
+              <div className='no-1'>
+              
+              <input type='password' id='senhac' placeholder= "CONFIRMAÇÃO DE SENHA" value={confirmaSenha} onChange={(e) => setConfirmaSenha(Number(e.target.value))}></input>
+              </div>
+            </div>
+
+
+            <div className='no-1'>
+              
+              <label>DATA DE NASCIMENTO</label>
+              <input type='date' id='dtna' value={dtNascimento} onChange={(e) => setDtNascimen(Number(e.target.value))}></input>
+              
+            </div>
+
+            <div className='c-6'>
+              <div className='no-1'>
+              <select className='tipodepele' value={tipoPele} onChange={e => setTipoPele(e.target.value)}>
+                <option id='tipopele' value={0}> Tipo Pele </option>
+                {
+                  tipoPeleSelecionado.map(item =>
+
+                    <option value={item.id}> {item.nome} </option>
+                  )
+
+                } 
+              </select>
+              </div>
+              </div>
+            
+                <Link to={'http://localhost:3000/login'}>
+                
+                <button className='botao'  onclick={cadastrar} >Cadastrar</button>
+                </Link>
+          </div>
+
+
+        </div>
+      </div>
+
+     
+
+    </div>
+  );
 }
