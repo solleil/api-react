2d2dd409fa3e010bd70108ed3a6b22a0bd087618
paloma <palomajonson207@gmail.com>
2023-11-17 18:00:58 -0300

    ok

--- site/src/constats.js
+++ site/src/constats.js
@@ -1,4 +1,4 @@
 
-// export const api_url = 'http://129.148.42.252:5027';
+export const api_url = 'http://129.148.42.252:5027';
 
-export const api_url = 'http://localhost:5000';
+//export const api_url = 'http://localhost:5000';
